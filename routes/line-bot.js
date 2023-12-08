8b46babbbee95dbf52ab0aeb3494561836e9fee3
kevinhws <kevinshu1995@gmail.com>
2023-12-08 11:32:18 +0800

    [fix] 回傳資料細節調整

--- routes/line-bot.js
+++ routes/line-bot.js
@@ -13,19 +13,16 @@ router.get("/", function (req, res, next) {
     res.status(200).send("Hello, this is line-bot server.");
 });
 
-router.post("/webhook", function (req, res) {
-    const events = req.body.events;
-    // Request header. See Messaging API reference for specification
-    const headers = {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + TOKEN,
-    };
-
-    events.forEach(event => {
-        if (event.type === "message" && event.mode === "active") {
-            if (event.message.type === "text") {
+router.post("/webhook", async function (req, res) {
+    const responses = await Promise.all(
+        req.body.events
+            .filter(event => event.type === "message" && event.mode === "active")
+            .filter(event => {
+                return event.message.type === "text";
+            })
+            .map(event => {
                 const userMessage = event.message.text;
-                axios
+                return axios
                     .post(
                         "https://api.line.me/v2/bot/message/reply",
                         {
@@ -43,24 +40,69 @@ router.post("/webhook", function (req, res) {
                             ],
                         },
                         {
-                            headers,
+                            headers: {
+                                "Content-Type": "application/json",
+                                Authorization: "Bearer " + TOKEN,
+                            },
                         }
                     )
                     .then(response => {
-                        console.log(response);
-                        res.status(200).send(response);
+                        return response;
                     })
                     .catch(error => {
-                        console.log(error);
-                        if (error.response) {
-                            res.status(error.response.status).send(error.response.response);
-                            return;
-                        }
-                        res.status(500).send(error);
+                        return error;
                     });
-            }
+            })
+    );
+
+    const isAllSuccess = responses.every(response => {
+        const status = response?.status;
+        if (status === undefined) return false;
+        return status >= 200 && status < 300;
+    });
+
+    if (isAllSuccess) {
+        res.status(200).send("OK");
+        return;
+    }
+
+    const firstStatus = responses.find(error => {
+        if (error?.response?.status) {
+            return error;
+        }
+    })?.status;
+
+    const responseError = responses.map(response => {
+        console.error(response);
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            return {
+                config: error.config,
+                status: error.response.status,
+                data: error.response.data,
+                headers: error.response.headers,
+                message: error.message,
+            };
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            return {
+                config: error.config,
+                request: error.request,
+                message: error.message,
+            };
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            return {
+                config: error.config,
+                message: error.message,
+            };
         }
     });
+
+    res.status(firstStatus ?? 500).send(responseError);
 });
 
 module.exports = router;
